fc16743fb2aba2869542bc913ad7ddec27276f47
Celia Chan <ccha92@uwo.ca>
2024-11-29 23:23:06 -0500

    trying to merge petClass

--- src/Bear.java
+++ src/Bear.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Bear extends Pet{ 
     public Bear(String name){
-        super(name, 100, 100, 40, 80, 0, "Bear"); //Temporary Stats
+        super(name, 100, 100, 40, 80, 0, "Bear"); 
     }
 
     @Override
@@ -12,8 +12,21 @@ public class Bear extends Pet{
         // Call the parent (Pet) render to reuse the same frame and panel
         super.render(frame, panel);
 
+        String spritePath = "images/bearnormal.png";
+        if(!isAlive()){
+            spritePath = "images/beardead.png";
+        } else if (isTired()){
+            spritePath = "images/beartired.png";
+        } else if (!isHappy()){
+            spritePath = "images/bearangry.png";
+        } else if(isHungry()){
+            spritePath = "images/bearhungry.png";
+        }else{
+            spritePath = "images/bearnormal.png";
+        }
+
         // Update sprite for the dog 
-        ImageIcon bearSprite = new ImageIcon("images/catbear.png"); // Replace with dog image path
+        ImageIcon bearSprite = new ImageIcon(spritePath); // Replace with bear image path
         JLabel spriteLabel = new JLabel(bearSprite);
         panel.add(spriteLabel, BorderLayout.CENTER);
 
